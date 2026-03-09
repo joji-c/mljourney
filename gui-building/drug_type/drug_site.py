a831dec6de9ac61300310abc8acbddb82383f263
joji <jojichakola@gmail.com>
2026-03-09 15:56:11 +0530

    change added

--- gui-building/drug_type/drug_site.py
+++ gui-building/drug_type/drug_site.py
@@ -6,18 +6,17 @@ st.title(":red[Drug type predictor:]")
 model = joblib.load("dt_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-Age = st.number_input("Enter Age", min_value=0)
-Male = st.number_input("Enter 1 if Male")
-Female = st.number_input("Enter 1 if Female")
-Low_Bp= st.number_input("Enter 1 if Low_Bp")
-Normal_Bp= st.number_input("Enter 1 if Normal_Bp")
-High_Bp= st.number_input("Enter 1 if High_Bp")
-Normal_Colestrol= st.number_input("Enter 1 if Normal_Colestrol")
-High_cholestrol= st.number_input("Enter 1 if High_colestrol")
+Age = st.slider("select age",1,100)
+Gender=st.selectbox("select Gender",["Male","Female"])
+Bp=st.selectbox("select Blood Preasure level",["High","Normal","Low"])
+Cholestrol=st.selectbox("select Cholestrol level",["High","Normal"])
 Na = st.number_input("Enter Sodium to Potassium level")
 
 if st.button("Predict Drug Type"):
-    test_data = [[Age,Male,Female,Low_Bp,Normal_Bp,High_Bp,Normal_Colestrol,High_cholestrol,Na]]
+    gender_map = {"Male":0, "Female":1}
+    bp_map = {"High":0, "Normal":1, "Low":2}
+    chol_map = {"High":0, "Normal":1}
+    test_data = [[Age,gender_map[Gender],bp_map[Bp],chol_map[Cholestrol],Na]]
     result = model.predict(scaler.transform(test_data))[0]
-    grade_map = { 0:'drugY', 1:'drugC', 2:'drugX', 3:'drugA', 4:'drugB'}
-    st.success(f"Predicted Drug: {grade_map[result]}")
+    grade_map = { 0:'DrugY', 1:'DrugC', 2:'DrugX', 3:'DrugA', 4:'DrugB'}
+    st.success(f"Drug Type : {grade_map[result]}")
